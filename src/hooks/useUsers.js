f7b9c67070c5918fa64e9a87f561e8e84b797fde
wknowpark797 <wknowpark797@gmail.com>
2023-08-07 12:44:46 +0900

    mutation 커스텀 훅 추가

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -1,5 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
+// 데이터 가져오기 Custom Hook
 const fetchUser = async ({ queryKey }) => {
 	const response = await fetch(`https://jsonplaceholder.typicode.com/users/${queryKey[1]}`);
 	return await response.json();
@@ -11,5 +12,30 @@ export const useUserQuery = (opt) => {
 		refetchOnMount: false,
 		staleTime: 1000 * 5,
 		cacheTime: 1000 * 5,
+	});
+};
+
+// 데이터 변경요청 후 가져오기 Custom Hook
+export const updateUserName = async (userName, num) => {
+	const response = await fetch(`https://jsonplaceholder.typicode.com/users/${num}`, {
+		method: 'PUT',
+		body: JSON.stringify({
+			title: userName,
+		}),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+	});
+
+	return await response.json();
+};
+
+export const useUpdateUserNameMutation = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation(updateUserName, {
+		onSuccess: (userName, num) => {
+			queryClient.setQueryData(['user', userName, num]);
+		},
 	});
 };
